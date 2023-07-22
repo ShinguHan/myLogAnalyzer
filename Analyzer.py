8a2287ae3d7d15978616b1272370db40a3c423f9
ShinguHan <wel2u@naver.com>
2023-07-22 16:15:32 +0900

    first

---
+++ Analyzer.py
@@ -0,0 +1,46 @@
+from PySide6.QtWidgets import QApplication, QComboBox, QVBoxLayout, QWidget
+from PySide6.QtWebEngineWidgets import QWebEngineView
+
+class MainWindow(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.combo_box = QComboBox()
+        self.combo_box.addItems(["AGV", "CNV", "STK"])
+        self.combo_box.currentTextChanged.connect(self.load_file)
+
+        self.web_view = QWebEngineView()
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.combo_box)
+        layout.addWidget(self.web_view)
+        self.setLayout(layout)
+
+    def load_file(self, text):
+        # Adjust this method to load the markdown file based on the selected category
+        with open(f"{text}.md", "r") as file:
+            content = file.read()
+
+        html = f"""
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <script src="https://unpkg.com/mermaid/dist/mermaid.min.js"></script>
+            <script>mermaid.initialize({{'startOnLoad':true}});</script>
+        </head>
+        <body>
+            <div class="mermaid">
+                {content}
+            </div>
+        </body>
+        </html>
+        """
+
+        self.web_view.setHtml(html)
+
+
+if __name__ == "__main__":
+    app = QApplication([])
+    window = MainWindow()
+    window.show()
+    app.exec()
